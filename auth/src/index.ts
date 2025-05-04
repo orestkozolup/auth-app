a9f10951683e26430c1e1008b6c9bf9099352ae3
Orest Kozolup <kozolup.yakiv@gmail.com>
2025-05-04 15:45:34 +0200

    add mongodb pod

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -23,6 +24,17 @@ app.use(async (req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Auth service listening on port: 3000")
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB")
+  } catch (e) {
+    console.error(e);
+  }
+
+  app.listen(3000, () => {
+    console.log("Auth service listening on port: 3000")
+  });
+}
+
+start();
