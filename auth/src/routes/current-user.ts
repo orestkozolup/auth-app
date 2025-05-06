2e7c0a3053fc2e15f5ab1eaf6a5ed276e2dee649
Orest Kozolup <kozolup.yakiv@gmail.com>
2025-05-06 21:52:44 +0200

    current user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("Success! current user!")
+  if (!req.session?.jwt) {
+    res.send({ currentUser: null })
+  }
+
+  try {
+    const payload = jwt.verify(req.session!.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload })
+  } catch (err) {
+    res.send({ currentUser: null })
+  }
 });
 
 export { router as currentUserRouter };
