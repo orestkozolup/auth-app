cf1e15f6186082040fd6c92c1901e316cd52bb52
Orest Kozolup <kozolup.yakiv@gmail.com>
2025-05-08 19:45:15 +0200

    sign up

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,57 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default () => {
-  return <form>
-    <h1>Sign Up</h1>
-    <div className="form-group">
-      <label>Email Address</label>
-      <input className="form-control" />
-    </div>
-    <div className="form-group">
-      <label>Password</label>
-      <input type="password" className="form-control" />
-    </div>
-    <button className="btn btn-primary">Sign Up</button>
-  </form>
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+
+      console.log("USER", user);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group mb-3">
+        <label>Email Address</label>
+        <input
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger mb-3">
+          <h4>Oops</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
