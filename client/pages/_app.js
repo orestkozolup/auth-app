d6afe540e43762ae711bc9809bceb0bdedc8124f
Orest Kozolup <kozolup.yakiv@gmail.com>
2025-05-10 13:15:06 +0200

    fetch in app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,34 @@
 import "bootstrap/dist/css/bootstrap.css";
 
-export default ({ Component, pageProps }) => {
+import buildClient from "../api/build-client";
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
-      <h1>Header</h1>
+      <h1>Header {currentUser?.email}</h1>
       <Component {...pageProps} />
     </div>
   );
 };
+
+AppComponent.getInitialProps = async (context) => {
+  try {
+    const { data } = await buildClient(context.ctx).get(
+      "/api/users/currentuser"
+    );
+
+    let pageProps;
+    if (context.Component.getInitialProps) {
+      pageProps = await context.Component.getInitialProps(context.ctx);
+    }
+
+    return {
+      pageProps,
+      ...data,
+    };
+  } catch (e) {
+    return { currentUser: null };
+  }
+};
+
+export default AppComponent;
